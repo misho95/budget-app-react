964a4a9d3f5dde5a72bf83474f82995276db9cd8
Misho <chapidze.misho95@gmail.com>
2023-06-17 21:39:10 +0400

    final

--- src/components/filterBar.jsx
+++ src/components/filterBar.jsx
@@ -1,11 +1,37 @@
-function FilterBar(){
+import { useRef } from "react";
+
+function FilterBar({data, setFilter}){
+
+    const date = useRef();
+    const category = useRef();
+    const min = useRef();
+    const max = useRef();
+    
+    const filterInvoice = (e) => {
+        e.preventDefault();
+        const cat = category.current.value;
+        const dat = date.current.value;
+        const minVal = min.current.value;
+        const maxVal = max.current.value;
+
+        const filterData = data.filter( (inv) => {
+            if(dat && dat === inv.date ||
+                cat && cat === inv.category ||
+                minVal && minVal < +inv.amount ||
+                maxVal && maxVal > +inv.amount
+                ) return inv;
+        })
+
+        setFilter(filterData);
+    }
+
     return(
         <div className="w-full flex justify-between">
-           <form id="filterForm" className="w-full flex flex-col sm:flex-row gap-3 justify-around items-center">
+           <form onSubmit={filterInvoice} className="w-full flex flex-col sm:flex-row gap-3 justify-around items-center">
 
-                    <input type="date" className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-600 rounded-md p-1 min-w-fit"/>
+                    <input ref={date} type="date" className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-600 rounded-md p-1 min-w-fit"/>
 
-                    <select className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-600 rounded-md p-1">
+                    <select ref={category} className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-600 rounded-md p-1">
                         <option>Any</option>
                         <option>shopping</option>
                         <option>gym</option>
@@ -14,9 +40,9 @@ function FilterBar(){
                         <option>other</option>
                     </select>
 
-                    <input type="text" placeholder="min" className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-600 rounded-md p-1"/>
+                    <input ref={min} type="text" placeholder="min" className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-600 rounded-md p-1"/>
 
-                    <input type="text" placeholder="max" className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-600 rounded-md p-1"/>
+                    <input ref={max} type="text" placeholder="max" className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-600 rounded-md p-1"/>
 
                 <button className="bg-purple-300 py-2 px-4 rounded-md text-purple-700 shadow-sm">Filter</button>
            </form>
--- src/home.jsx
+++ src/home.jsx
@@ -1,15 +1,19 @@
+import { useState, useEffect } from "react";
 import FilterBar from "./components/filterBar";
 import TopStats from './components/topstats';
 import Invoices from './components/invoices';
 import MidStats from "./components/midstats";
 
 function Home( {data, deleteInv} ){
+
+    const [ filter, setFilter ] = useState([]);
+    
     return(
         <>
-            <FilterBar />
+            <FilterBar data={data} setFilter={setFilter}/>
             <TopStats data={data} />
             <MidStats data={data} />
-            <Invoices data={data} deleteInv={deleteInv} />
+            <Invoices data={filter.length > 0 ? filter : data} deleteInv={deleteInv} />
         </>
     )
 }
